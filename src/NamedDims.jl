e3277ea227035dc73cf6b6837c51bc0798d334cb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-01 13:00:35 +0000

    Snapshot of branch master taken on 2019-07-01T09:00:34.101-04:00 from "https://github.com/UnofficialJuliaMirror/NamedDims.jl-356022a1-0364-5f58-8944-0da4b18d706f"

--- src/NamedDims.jl
+++ src/NamedDims.jl
@@ -3,9 +3,19 @@ using Base: @propagate_inbounds
 using Base.Broadcast:
     Broadcasted, BroadcastStyle, DefaultArrayStyle, AbstractArrayStyle, Unknown
 using LinearAlgebra
+using Pkg
+using Requires
 using Statistics
 
-export NamedDimsArray, dim, rename
+export NamedDimsArray, dim, rename, unname
+
+function __init__()
+    # NOTE: NamedDims is only compatible with Tracker v0.2.2; but no nice way to enforce that.
+    @require Tracker = "9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" include("tracker_compat.jl")
+end
+
+# We use CoVector to workout if we are taking the tranpose of a tranpose etc
+const CoVector = Union{Adjoint{<:Any, <:AbstractVector}, Transpose{<:Any, <:AbstractVector}}
 
 include("name_core.jl")
 include("wrapper_array.jl")
@@ -14,5 +24,4 @@ include("functions.jl")
 include("functions_dims.jl")
 include("functions_math.jl")
 
-include("compat.jl")
 end # module
